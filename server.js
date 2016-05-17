0255a2fd23b201cb26a7d94b6e4074e13838d790
Paulo Panissa <p.panissa@gmail.com>
2016-05-17 10:10:30 -0400

    somente socket.io

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var Redis = require('ioredis');
-var redis = new Redis();
 var load = require('express-load');
 
 load('sockets', {cwd: 'public'})
-    .into(io, redis);
+    .into(io);
 
 http.listen(3000, function(){
     console.log('Listening on Port 3000');
